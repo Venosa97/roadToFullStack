37a6899d57b55160fd0d377f93150e347edaef24
unknown <costivn@gmail.com>
2022-03-12 13:48:31 +0200

    added homework for curs-5

--- curs-5/src/com/devmind/SpringApp/temaCurs5/Main.java
+++ curs-5/src/com/devmind/SpringApp/temaCurs5/Main.java
@@ -1,2 +1,15 @@
-package com.devmind.SpringApp.temaCurs5;public class Main {
+package com.devmind.SpringApp.temaCurs5;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+//        Task protoFirstTask = context.getBean("prototypeTask",Task.class);
+//        Task protoSecondTask = context.getBean("prototypeTask", Task.class);
+//        protoFirstTask.run();
+        Task singleTask = context.getBean("singletonTask", Task.class);
+        context.close();
+
+    }
 }
--- curs-5/src/com/devmind/SpringApp/temaCurs5/Task.java
+++ curs-5/src/com/devmind/SpringApp/temaCurs5/Task.java
@@ -1,2 +1,44 @@
-package com.devmind.SpringApp.temaCurs5;public class Task {
+package com.devmind.SpringApp.temaCurs5;
+
+import org.springframework.util.SocketUtils;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Task {
+    static int uniqueID = 0;
+    static int numberOfTasks = 0;
+    private String taskName;
+    private Integer taskId;
+    private Integer executionTime;
+
+
+    public void run(){
+        System.out.println("Run method: ");
+        try{
+            Thread.sleep(executionTime);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+    public void init(){
+        System.out.println("Starting init method");
+        System.out.println("numberOfTasks: " + ++numberOfTasks);
+        this.taskId = Task.generateId();
+        System.out.println("taskId: " + taskId);
+        this.taskName = "Task" + " " + taskId;
+        System.out.println("taskName: " + taskName);
+        this.executionTime = ThreadLocalRandom.current().nextInt(1, 20 + 1);
+        System.out.println("executionTime: " + this.executionTime);
+        System.out.println("--------------------\n");
+    }
+
+    public void destroy(){
+        System.out.println("Destroy task: " + this.taskName);
+        System.out.println("Tasks in progress: " + --numberOfTasks);
+    }
+    public static Integer generateId(){
+        return ++uniqueID;
+    }
 }
